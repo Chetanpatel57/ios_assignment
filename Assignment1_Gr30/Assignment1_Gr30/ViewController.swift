ca5e3e5a7e35098a0e7e56bf94834400b4d702c4
Chetanpatel57 <40812586+Chetanpatel57@users.noreply.github.com>
2022-09-26 23:54:43 -0400

    Add files via upload

---
+++ Assignment1_Gr30/Assignment1_Gr30/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment1_Gr30
+//
+//  Created by Sriskandarajah Sayanthan on 2022-09-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
